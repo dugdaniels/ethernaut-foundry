cfe022ebad019183e28763cb76476db5054c4033
Corey Daniels <c.daniels@alkime.co>
2022-12-23 21:29:46 -0500

    Clean up level 14

--- src/level14.sol
+++ src/level14.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../instances/Ilevel13.sol";
+import "../instances/Ilevel14.sol";
 
 contract Gatecrasher {
     constructor(address _gate) {
         bytes8 key = bytes8(uint64(bytes8(keccak256(abi.encodePacked(this)))) ^ type(uint64).max);
-        GatekeeperOne(_gate).enter(key);
+        GatekeeperTwo(_gate).enter(key);
     }
 }
